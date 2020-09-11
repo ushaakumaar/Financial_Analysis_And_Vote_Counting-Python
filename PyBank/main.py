93245e41be70fd48a47006f806ce0edfe6c17208
Usha Saravanakumar <67562186+ushaakumaar@users.noreply.github.com>
2020-09-11 11:38:41 -0400

    Logic to retrieve and save months/revenue data in list

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,10 +15,22 @@ with open(budgetCSV, "r") as csvFile:
     # variable to hold the total number of months
     totalMonths = 0
 
+    # list variable to hold the months
+    months = []
+
+    # list variable to hold the revenue(profit/losses)
+    revenue = []
+
     # read through each row of data after the header
     for row in csvReader:
 
-        #increment total months by 1 for every row read
+        # increment total months by 1 for every row read
         totalMonths += 1
+        
+        # store month data in a list
+        months.append(row[0])
+
+        # store revenue data in a list
+        revenue.append(row[1])
 
 print(totalMonths)
